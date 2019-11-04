1227bdfdfbb315cfe016e80cd1793d8ac802b9b9
Joel Van Horn <joel@joelvanhorn.com>
2019-11-04 15:00:31 -0500

    Fixes function binding

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import limitPromises from '@uppy/utils/lib/limitPromises'
 import settle from '@uppy/utils/lib/settle'
 
 export default class AwsAmplify extends Plugin {
-  constructor (uppy, opts) {
+  constructor(uppy, opts) {
     super(uppy, opts)
 
     this.type = 'uploader'
@@ -17,43 +17,44 @@ export default class AwsAmplify extends Plugin {
       }
     }
 
-    const defaultOptions = {
+    this.opts = {
       // AWS Amplify `Storage` reference
       storage: null, // { put(), get() }
       getOptions: { download: false },
       limit: 0,
       getUploadParameters: this.getUploadParameters.bind(this),
-      locale: defaultLocale
+      locale: defaultLocale,
+      ...opts
     }
 
-    this.opts = { ...defaultOptions, ...opts }
-
     // i18n
-    this.translator = new Translator([ defaultLocale, this.uppy.locale, this.opts.locale ])
-    this.i18n = this.translator.translate.bind(this.translator)
-    this.i18nArray = this.translator.translateArray.bind(this.translator)
-
-    this.handleUpload = this.handleUpload.bind(this)
+    this.translator = new Translator([
+      defaultLocale,
+      this.uppy.locale,
+      this.opts.locale
+    ])
+    this.i18n = (...args) => this.translator.translate(...args)
+    this.i18nArray = (...args) => this.translator.translateArray(...args)
 
     if (typeof this.opts.limit === 'number' && this.opts.limit !== 0) {
       this.limitRequests = limitPromises(this.opts.limit)
     } else {
-      this.limitRequests = (fn) => fn
+      this.limitRequests = fn => fn
     }
   }
 
-  async getUploadParameters (file) {
+  async getUploadParameters(file) {
     return {
       filename: `${Date.now()}-${file.name}`
     }
   }
 
-  validateParameters (file, params) {
+  validateParameters(file, params) {
     return params
   }
 
-  prepareUpload (fileIDs) {
-    fileIDs.forEach((id) => {
+  prepareUpload(fileIDs) {
+    fileIDs.forEach(id => {
       this.uppy.emit('preprocess-progress', this.uppy.getFile(id), {
         mode: 'determinate',
         message: this.i18n('preparingUpload'),
@@ -61,7 +62,9 @@ export default class AwsAmplify extends Plugin {
       })
     })
 
-    const getUploadParameters = this.limitRequests(this.opts.getUploadParameters)
+    const getUploadParameters = this.limitRequests(
+      this.opts.getUploadParameters
+    )
 
     return Promise.all(
       fileIDs.map(id => {
@@ -76,36 +79,41 @@ export default class AwsAmplify extends Plugin {
               value: 1
             })
             return { ...params, id }
-          }).catch(error => {
+          })
+          .catch(error => {
             this.uppy.emit('upload-error', file, error)
           })
       })
     ).then(paramsCollection => {
-      fileIDs.forEach(id => this.uppy.emit('preprocess-complete', this.uppy.getFile(id)))
+      fileIDs.forEach(id =>
+        this.uppy.emit('preprocess-complete', this.uppy.getFile(id))
+      )
       return paramsCollection
     })
   }
 
-  handleFileUpload (filename, file, position, total) {
+  handleFileUpload(filename, file, position, total) {
     this.uppy.log(`[AwsAmplify] Uploading ${position} of ${total}`)
 
     const { storage, getOptions } = this.opts
 
-    return storage.put(filename, file.data, {
-      contentType: file.type,
-      progressCallback (progress) {
-        if (!this.uppy.getFile(file.id)) {
-          throw new Error('File removed?')
+    return storage
+      .put(filename, file.data, {
+        contentType: file.type,
+        progressCallback(progress) {
+          if (!this.uppy.getFile(file.id)) {
+            throw new Error('File removed?')
+          }
+          this.uppy.emit('upload-progress', file, {
+            uploader: this,
+            bytesUploaded: progress.loaded,
+            bytesTotal: progress.total
+          })
         }
-        this.uppy.emit('upload-progress', file, {
-          uploader: this,
-          bytesUploaded: progress.loaded,
-          bytesTotal: progress.total
-        })
-      }
-    })
+      })
       .then(body => {
-        return storage.get(body.key, getOptions)
+        return storage
+          .get(body.key, getOptions)
           .then(uploadURL => {
             this.uppy.emit('upload-success', file, {
               body,
@@ -123,7 +131,7 @@ export default class AwsAmplify extends Plugin {
       })
   }
 
-  handleUpload (fileIDs) {
+  handleUpload = fileIDs => {
     if (fileIDs.length === 0) {
       this.uppy.log('[AwsAmplify] No files to upload!')
       return Promise.resolve()
@@ -131,30 +139,37 @@ export default class AwsAmplify extends Plugin {
 
     this.uppy.log('[AwsAmplify] Uploading...')
 
-    return this.prepareUpload(fileIDs).then(paramsCollection => {
-      return paramsCollection.map(({ id, filename }, index) => {
-        const file = this.uppy.getFile(id)
+    return this.prepareUpload(fileIDs)
+      .then(paramsCollection => {
+        return paramsCollection.map(({ id, filename }, index) => {
+          const file = this.uppy.getFile(id)
 
-        if (file.error) {
-          return () => Promise.reject(new Error(file.error))
-        } else {
+          if (file.error) {
+            return () => Promise.reject(new Error(file.error))
+          }
+
           // We emit upload-started here, so that it's also emitted for files
           // that have to wait due to the `limit` option.
           this.uppy.emit('upload-started', file)
-          return this.handleFileUpload.bind(this, filename, file, index + 1, paramsCollection.length)
-        }
+          return () =>
+            this.handleFileUpload(
+              filename,
+              file,
+              index + 1,
+              paramsCollection.length
+            )
+        })
       })
-    })
       .then(actions => actions.map(action => this.limitRequests(action)()))
       .then(promises => settle(promises))
       .then(() => null)
   }
 
-  install () {
+  install() {
     this.uppy.addUploader(this.handleUpload)
   }
 
-  uninstall () {
+  uninstall() {
     this.uppy.removeUploader(this.handleUpload)
   }
 }
